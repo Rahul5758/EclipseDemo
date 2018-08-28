ae6b166230efcb7cb7cc3d0d7a7a59b5c53c4eb6
Rahul5758 <rahulthapliyal5758@gmail.com>
2018-08-28 19:03:18 +0530

    Pushed

--- Hello/src/m.java
+++ Hello/src/m.java
@@ -4,5 +4,7 @@ public class m {
 	int x;
 	public static void main(String[] args) {
 		
-	System.out.println("Have fun Magister");}
+	System.out.println("Have fun Magister");
+	System.out.println("Second commit in progress");
+	}
 }
